c1af1066767b5e28ed1180181f1b89cddb329054
pantovicnevena <nevena.pantovic1998@gmail.com>
2021-06-27 21:41:06 +0200

    Accept giveaway updated

--- DarivanjeAppFront/src/app/list-of-giveaways/list-of-giveaways.page.ts
+++ DarivanjeAppFront/src/app/list-of-giveaways/list-of-giveaways.page.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Darivanje } from '../shared/models/darivanje.model';
 import { DarivanjeService } from '../shared/services/darivanje.service';
+
 
 @Component({
   selector: 'app-list-of-giveaways',
@@ -8,16 +12,30 @@ import { DarivanjeService } from '../shared/services/darivanje.service';
   styleUrls: ['./list-of-giveaways.page.scss'],
 })
 export class ListOfGiveawaysPage implements OnInit {
-  public darivanja: any;
+  public darivanja: Array<any> = [];
 
-  constructor(private darivanjeService: DarivanjeService) {
+  constructor(private darivanjeService: DarivanjeService, private router: Router, activeRoute: ActivatedRoute) {
     this.darivanjeService.VratiNaCekanju()
         .subscribe(darivanja => {
           console.log(darivanja);
+          this.darivanja = darivanja;
         });
   }
 
   ngOnInit() {
+  }
+
+  public Odobri(d: Darivanje): void {
+    this.darivanjeService.OdobriDarivanje(d);
+
+    this.router.navigate([this.router]);
+  }
+
+  reloadCurrentPage(){
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+    this.router.navigate([currentUrl]);
+    });
   }
 
 }
--- DarivanjeAppFront/src/app/shared/services/darivanje.service.ts
+++ DarivanjeAppFront/src/app/shared/services/darivanje.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Darivanje } from '../models/darivanje.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,17 @@ export class DarivanjeService {
 
   public VratiNaCekanju(): Observable<any>{
     return this.http.get<any>('https://localhost:44328/giveaways');
+  }
+
+  public OdobriDarivanje(darivanje: Darivanje){
+    return this.http.patch<any>('https://localhost:44328/approve-giveaway', darivanje)
+    .subscribe(
+      (response) => {
+        console.log(response.toString());
+      },
+      (error) => {
+        console.log(error.toString());
+      }
+    );
   }
 }
